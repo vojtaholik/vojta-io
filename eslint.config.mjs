50cbdd7a3cd5f0e8b729c9b2153ff1200cfcbd31
Vojta Holik <vojta@egghead.io>
2025-07-08 15:01:37 +0200

    Update ESLint config to disable rules for unescaped entities and unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ]
 
 export default eslintConfig
